0658d41a6821d2983fb6d619a9dbea9166cd5f57
educacion <educacion@blanca>
2018-05-14 12:46:30 -0300

    con el test hicimos la compra

--- Clase14OOPIniciando/src/pruebas/Compra.java
+++ Clase14OOPIniciando/src/pruebas/Compra.java
@@ -1,5 +1,33 @@
 package pruebas;
 
 class Compra {
-    
+
+    private int total;
+    private boolean delivery;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public boolean isDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(boolean delivery) {
+        this.delivery = delivery;
+    }
+
+    void imprmirTotal() {
+        if (this.isDelivery()) {
+            System.out.println("Total Final con Delivery : " + ( total + ( total * 0.1 ) ));
+//            System.out.println("Total Final: " + ( total * 1.1  ));
+        } else {
+            System.out.println("Total Final sin Delivery : " + total);
+        }
+    }
+
 }
--- Clase14OOPIniciando/src/pruebas/TestCompras.java
+++ Clase14OOPIniciando/src/pruebas/TestCompras.java
@@ -19,15 +19,37 @@ public class TestCompras {
         
         // crear clase de Compra
         Compra pizza;
+        
         // Instanciar clase de Compra
+        pizza = new Compra(); // aqui esta el constructor
         
         // asignar valor total de compra
+        pizza.setTotal(200);
+        System.out.println("la pizza : " + pizza.getTotal() );
+        
+        pizza.setDelivery(true);
+        
         // imprimirTotal() programar el metodo
         //    que agrega un 10% si es por delivery
         // por lo tanto, 
         //    agregar un atributo delivery true o false
-        
+        pizza.imprmirTotal();
+
+        System.out.println("1. termine caso de prueba CON delivery");
+        Compra hamburguesa = new Compra();
+        hamburguesa.setTotal(230);
+        hamburguesa.setDelivery(false);
+        System.out.println("la hamburguesa: " +  hamburguesa.getTotal());
+        hamburguesa.imprmirTotal();
+        System.out.println("2. termine caso de prueba SIN delivery");
         
         System.out.println("[OK] Test Comprando");
     }
 }
+
+
+
+
+
+
+
