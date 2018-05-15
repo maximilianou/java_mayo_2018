42705390966f33a8824a22db72f4befc3a911beb
educacion <educacion@blanca>
2018-05-15 11:49:04 -0300

    agregue encapsulamiento con excepcion

--- Clase14OOP/src/pruebas/Diagnostico.java
+++ Clase14OOP/src/pruebas/Diagnostico.java
@@ -1,7 +1,37 @@
 package pruebas;
+// package 
 
-class Diagnostico {
+// clase
+class Diagnostico  {
 
-    String codigoUnico;
+    // atributos
+    private String codigoUnico;
+    private String paciente;
+
+    // metodo, polimorfismo por redefinicion
+    @Override
+    public String toString() {
+        return "" + getCodigoUnico() + "::" + getPaciente() ;
+    }
+    // Encapsulamimento
+
+    public String getCodigoUnico() {
+        return codigoUnico;
+    }
+    public void setCodigoUnico(String codigoUnico) {
+        if( codigoUnico.isEmpty()){
+            throw new RuntimeException("Pifiaste en el codigo!![" + codigoUnico + "]");
+        }
+        this.codigoUnico = codigoUnico;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+    
+
     
 }
--- Clase14OOP/src/pruebas/TestDiagnostico.java
+++ Clase14OOP/src/pruebas/TestDiagnostico.java
@@ -1,23 +1,57 @@
 package pruebas;
 
-/** TDD: Test Driven Development
- * 
- **/
+/**
+ * TDD: Test Driven Development
+ *
+ *
+ */
 public class TestDiagnostico {
+
     public static void main(String[] args) {
         System.out.println("[..] Diagnostico !!");
-        
+
         // crear un diagnostico
         // el diagnositco es de pacientes
         // todos los diatnositicos son iguales
         // el diagonosito es un rotulo
         //  - un diagnositco es de 1 unico pacientes
         //  - ( despues sigo preguntando, quiero cobrar, quiero algo que ande)
-        
         Diagnostico enfTamy = new Diagnostico();
-        enfTamy.codigoUnico = "EXP123420180515";
-        System.out.println("Codigo Unico: " + enfTamy.codigoUnico);
-                
+        enfTamy.setCodigoUnico("EXP123420180515");
+        System.out.println("Codigo Unico: " + enfTamy.getCodigoUnico());
+
+        Diagnostico enfNaty = new Diagnostico();
+        enfNaty.setCodigoUnico("EXP888820180515");
+        System.out.println("Codigo Unico: " + enfNaty.getCodigoUnico());
+
+        Diagnostico enfEly = new Diagnostico();
+        enfEly.setCodigoUnico("EXP111120180515");
+        System.out.println("Codigo Unico: " + enfEly.getCodigoUnico());
+
+        enfTamy.setPaciente("Tamara");
+        enfNaty.setPaciente("Natalia");
+        enfEly.setPaciente("Elizabeth");
+
+        System.out.println("" + enfTamy);
+        System.out.println("" + enfNaty);
+        System.out.println("" + enfEly);
+        System.out.println("" + enfTamy
+                .toString() );
+        System.out.println("" + enfNaty.toString() );
+        System.out.println("" + enfEly.toString() );
+        System.out.println("1. --------------");
+        // verifico que el codigo no puede ser vacio
+        try{
+            enfTamy.setCodigoUnico("");
+            System.out.println("Mira PUDE PONER VACIO EL CODIGO" );
+        }catch(Exception e){
+            System.out.println("Verificate Esto: " + e.getMessage());
+        }
+        System.out.println("2. --------------");
+        System.out.println("3. --------------");
+        System.out.println("4. --------------");
+        System.out.println("5. --------------");
+        
         System.out.println("[OK] Diagnostico !!");
     }
 }
